a1eebc686044535cbc535da4f28c2b3e30edfa04
Musadiq Khan <musadiq.khan@gmail.com>
2020-06-07 17:24:21 +0500

    Update Navigation.js

--- src/Components/Navigation.js
+++ src/Components/Navigation.js
@@ -45,24 +45,6 @@ const Navigation = props => {
         </li>
       </ul>
     </section>
-    // <nav
-    //   style={{
-    //     display: "flex",
-    //     justifyContent: "flex-end",
-    //     backgroundColor: "black",
-    //   }}
-    // >
-    //   <p className=" ">Home</p>
-    //   <Link to="/#Features">
-    //     <p className="">Features</p>
-    //   </Link>
-    //   <Link to="/#About">
-    //     <p className="">About</p>
-    //   </Link>{" "}
-    //   <Link to="/#Join">
-    //     <p className="">Join Beta</p>
-    //   </Link>{" "}
-    // </nav>
   )
 }
 
